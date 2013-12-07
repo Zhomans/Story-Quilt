6753552a168e98f49ea1a92f6176702cfdcf4fa1
Mac-I Crowell <macicrowell@gmail.com>
2013-12-07 10:30:36 -0500

    trying to sync

--- StoryQuilt/src/main/java/com/roomates/storyquilt/MainActivity.java
+++ StoryQuilt/src/main/java/com/roomates/storyquilt/MainActivity.java
@@ -61,17 +61,14 @@ public class MainActivity extends Activity implements GooglePlayServicesClient.C
         setContentView(R.layout.activity_main);
 
         mPlusClient = new PlusClient.Builder(this, this, this)
-                //.setActions("http://schemas.google.com/CreateActivity") //my (Mac-I) phone always crashes on this saying : "java.lang.NoSuchMethodError: Lcom/google/android/gms/plus/PlusClient$Builder;.setActions"
+                //.setActions("http://schemas.google.com/CreateActivity"); //my (Mac-I) phone always crashes on this saying : "java.lang.NoSuchMethodError: Lcom/google/android/gms/plus/PlusClient$Builder;.setActions"
                 .setScopes(Scopes.PLUS_LOGIN)  // Space separated list of scopes
                 .build();
         mConnectionProgressDialog = new ProgressDialog(this);
         mConnectionProgressDialog.setMessage("Signing in...");
         //Check if logged in
         username = getUserName(); //getUserName();
-        if (username.equals("")) {
-            setUserName("readonly");
-            signIn();
-        } else if (username.equals("readonly")) {
+        if (username.equals("readonly")) {
             Toast.makeText(this, "You may only read stories, please sign in to contribute", Toast.LENGTH_LONG).show();
         }
         //Set up MainActivity Views
@@ -84,6 +81,15 @@ public class MainActivity extends Activity implements GooglePlayServicesClient.C
     protected void onStart() {
         super.onStart();
         mPlusClient.connect();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (username.equals("")) {
+            setUserName("readonly");
+            signIn();
+        }
     }
 
     @Override
