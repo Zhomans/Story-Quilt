c795de2fe7ec4ee88267cfdc52729ec8e27bb46c
Chris Lee <sihrc.c.lee@gmail.com>
2013-12-15 21:04:16 -0500

    fixed email is empty

--- StoryQuilt/src/main/java/com/roomates/storyquilt/ActivityStoryView.java
+++ StoryQuilt/src/main/java/com/roomates/storyquilt/ActivityStoryView.java
@@ -116,7 +116,7 @@ public class ActivityStoryView  extends Activity {
                     if (newPostText != null || !newPostText.toString().equals("")){
                         if (curStory.checkWordCount(newPostText.toString())){
                             //Add new Piece
-                            Piece newPiece = new Piece(getSharedPreferences("StoryQuilt", MODE_PRIVATE).getString("personEmail", ""), String.valueOf(System.currentTimeMillis()), newPostText.toString());
+                            Piece newPiece = new Piece(getSharedPreferences("StoryQuilt", MODE_PRIVATE).getString("email", ""), String.valueOf(System.currentTimeMillis()), newPostText.toString());
                             curStory.addPiece(newPiece);
                             //Make User a Writer if New
                             if (!userHandler.isWriter(curStory.id)){
