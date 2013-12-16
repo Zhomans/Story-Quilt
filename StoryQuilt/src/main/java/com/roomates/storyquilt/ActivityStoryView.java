9f8f9c1b69a2a3e1c4ec921ed0530be858e95bc0
Madison May <madison.may@students.olin.edu>
2013-12-15 20:52:52 -0500

    Fix leave button visibility issue

--- StoryQuilt/src/main/java/com/roomates/storyquilt/ActivityStoryView.java
+++ StoryQuilt/src/main/java/com/roomates/storyquilt/ActivityStoryView.java
@@ -170,6 +170,8 @@ public class ActivityStoryView  extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.story, menu);
         this.menu = menu;
+        menu.findItem(R.id.join_story).setVisible(userHandler.isReader(curStory.id));
+        menu.findItem(R.id.leave_story).setVisible(userHandler.isWriter(curStory.id));
         return true;
     }
 
