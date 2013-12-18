424ef3b8dcc3c7c4a38757ff463066cfe5f6b7d3
Madison May <madison.may@students.olin.edu>
2013-12-18 15:49:49 -0500

    Remove unneeded imports

--- StoryQuilt/src/main/java/com/roomates/storyquilt/ActivityMainTab.java
+++ StoryQuilt/src/main/java/com/roomates/storyquilt/ActivityMainTab.java
@@ -2,22 +2,15 @@ package com.roomates.storyquilt;
 
 import android.app.ActionBar;
 import android.app.Activity;
-import android.app.Fragment;
-import android.content.Context;
 import android.content.Intent;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
-import android.widget.EditText;
-import android.widget.FrameLayout;
-import android.widget.Toast;
+import android.widget.EditText;;
 
 public class ActivityMainTab extends ActivityGooglePlus {
     //Passing Menu from onCreateOptionsMenu to edit in onConnectionStatusChanged
--- StoryQuilt/src/main/java/com/roomates/storyquilt/FragmentAllStories.java
+++ StoryQuilt/src/main/java/com/roomates/storyquilt/FragmentAllStories.java
@@ -7,7 +7,6 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
